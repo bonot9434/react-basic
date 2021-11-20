5bf6fc09571fb5c876f1515f44c682599b1a2ac9
bonot9434 <bonot9434@gmail.com>
2021-11-20 23:33:54 +0900

    textフォーム作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Article from "./components/Article"
+import {TextInput}from"./components";
 
 function App() {
   return (
     <div>
+      <TextInput />
       <Article
         title={'タイトル欄'}
         content={'コンテント欄'}
