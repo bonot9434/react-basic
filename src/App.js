1a59a12801f1a25efd7031a614048bae65794fcf
bonot9434 <bonot9434@gmail.com>
2021-11-21 00:01:47 +0900

    カウンター作成

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Article from "./components/Article"
-import {TextInput}from"./components";
+import {TextInput, Counter}from"./components";
 
 function App() {
   return (
     <div>
-      <TextInput />
       <Article
         title={'タイトル欄'}
         content={'コンテント欄'}
       />
+      <TextInput />
+      <Counter />
     </div>
   );
 }
